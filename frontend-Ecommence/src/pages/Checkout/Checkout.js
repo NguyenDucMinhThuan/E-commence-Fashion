67e56e4fd345fe75fa9aa117401911f1c608e0f2
NguyenDucMinhThuan <nguyenducminhthuan012@gmail.com>
2024-04-18 14:26:19 +0700

    Order: Done

--- frontend-Ecommence/src/pages/Checkout/Checkout.js
+++ frontend-Ecommence/src/pages/Checkout/Checkout.js
@@ -1,153 +1,152 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import classNames from 'classnames/bind';
 import styles from './Checkout.module.scss';
+import Image from '~/components/Image';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBackspace, faCheck, faShirt, faShop, faStar, faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleDot, faTag } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
-const images = [
-    'https://pos.nvncdn.com/d0f3ca-7136/ps/20230313_Nok6q2koiS.jpeg',
-    'https://pos.nvncdn.com/d0f3ca-7136/ps/20230313_K0bKAhvbrb.jpeg',
-    'https://pos.nvncdn.com/d0f3ca-7136/ps/20230313_H97BYyaRmF.jpeg',
-];
 
 function Checkout() {
-    const [colorStates, setColorStates] = useState({
-        white: false,
-        black: false,
-        yellow: false,
-    });
-    const [sizeStates, setSizeStates] = useState({
-        M: false,
-        L: false,
-        XL: false,
-    });
-
-    const checkSize = (size) => {
-        setSizeStates((prevState) => ({
-            ...prevState,
-            [size]: !prevState[size],
-        }));
-    };
-    const checkColor = (color) => {
-        setColorStates((prevState) => ({
-            ...prevState,
-            [color]: !prevState[color],
-        }));
-    };
-
     return (
         <div className={cx('Wrapper')}>
-            <div className={cx('headline')}>Trang chủ / Phụ kiện / Nón / Bucket</div>
-            <div className={cx('about-product')}>
-                <div className={cx('info-product')}>
-                    <div className={cx('name-product')}>NÓN BUCKET JEAN UNISEX - TOTODAY - PLAIN COLOR</div>
-                    <div className={cx('estimate')}>
-                        <div className={cx('rate')}>
-                            <FontAwesomeIcon icon={faStar} className={cx('star')} />
-                            <p className={cx('rate-star')}>5/5</p>
-                        </div>
-                        <span className={cx('rate-count')}>Đã bán : 20</span>
+            <div className={cx('headline')}>
+                <div className={cx('headline-item')}>
+                    <div className={cx('step')}>1</div>
+                    <div className={cx('step-text')}>Giỏ hàng</div>
+                </div>
+                <div className={cx('line')}></div>
+                <div className={cx('headline-item')}>
+                    <div className={cx('step')}>2</div>
+                    <div className={cx('step-text')}>Đặt hàng</div>
+                </div>
+                <div className={cx('line')}></div>
+                <div className={cx('headline-item')}>
+                    <div className={cx('step')}>3</div>
+                    <div className={cx('step-text')}>Xác nhận</div>
+                </div>
+            </div>
+            <div className={cx('order')}>
+                <div className={cx('order-title')}>
+                    <div className={cx('order-title-text')}>ĐẶT HÀNG</div>
+                    <div className={cx('order-title-checklogin')}>
+                        Bạn đã có tài khoản?
+                        <span className={cx('order-title-login')}>Đăng nhập ngay</span>
                     </div>
-                    <div className={cx('price-product')}>160.000đ</div>
-                    <div className={cx('color-product')}>
-                        <div className={cx('color-text')}>Màu sắc</div>
-                        <div className={cx('color-choose')}>
-                            <ul className={cx('ul')} style={{ flexWrap: 'wrap' }}>
-                                {images.map((index) => (
-                                    <li className={cx('li-color')}>
-                                        <div
-                                            className={cx('div-checkbox-color', colorStates.white && 'checked-Color')}
-                                            id="white"
-                                            style={{ backgroundColor: 'white' }}
-                                            onClick={() => checkColor('white')}
-                                        >
-                                            <img key={index} className={cx('item-image')} src={index} alt="" />
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
+                </div>
+                <div className={cx('information')}>
+                    <div className={cx('info-customer')}>
+                        <div className={cx('info-customer-text')}>Thông tin nhận hàng</div>
+                        <div className={cx('info-name')}>
+                            <label>Họ tên</label>
+                            <input className={cx('input-hoten')} placeholder="Họ tên" />
                         </div>
-                    </div>
-                    <div className={cx('size-product')}>
-                        <div className={cx('size')}>
-                            <div className={cx('size-text')}>Size</div>
-                            <div className={cx('size-choose')}>
-                                <ul className={cx('ul')} style={{ flexWrap: 'wrap' }}>
-                                    <li className={cx('li-size')}>
-                                        <div
-                                            className={cx('div-checkbox-size', sizeStates.M && 'checked-Color')}
-                                            id="M"
-                                            onClick={() => checkSize('M')}
-                                        >
-                                            M
-                                        </div>
-                                    </li>
-                                    <li className={cx('li-size')}>
-                                        <div
-                                            className={cx('div-checkbox-size', sizeStates.L && 'checked-Color')}
-                                            id="L"
-                                            onClick={() => checkSize('L')}
-                                        >
-                                            L
-                                        </div>
-                                    </li>
-                                    <li className={cx('li-size')}>
-                                        <div
-                                            className={cx('div-checkbox-size', sizeStates.XL && 'checked-Color')}
-                                            id="XL"
-                                            onClick={() => checkSize('XL')}
-                                        >
-                                            XL
-                                        </div>
-                                    </li>
-                                </ul>
+                        <div className={cx('email-sdt')}>
+                            <div>
+                                <label>Số điện thoại</label>
+                                <input className={cx('input-sdt')} placeholder="Số điện thoại" />
+                            </div>
+                            <div>
+                                <label>Email</label>
+                                <input className={cx('input-email')} placeholder="Email" />
                             </div>
                         </div>
-                        <div className={cx('size-chart')}>
-                            <FontAwesomeIcon icon={faShirt} className={cx('icon-commit')} />
-                            Size chart
+                        <div className={cx('')}>
+                            <label>Địa chỉ nhận hàng</label>
+                            <input className={cx('input-address')} placeholder="Địa chỉ nhận hàng" />
+                        </div>
+                        <div className={cx('detail-address')}>
+                            <div className={cx('city')}>
+                                <label>Tỉnh / Thành phố</label>
+                                <input className={cx('input-city')} placeholder="Tỉnh / Thành phố" />
+                            </div>
+                            <div className={cx('district')}>
+                                <label>Quận / Huyện</label>
+                                <input className={cx('input-district')} placeholder="Quận / Huyện" />
+                            </div>
+                            <div className={cx('ward')}>
+                                <label>Phường / Xã</label>
+                                <input className={cx('input-ward')} placeholder="Phường / Xã" />
+                            </div>
+                        </div>
+                        <div className={cx('')}>
+                            <label>Ghi chú</label>
+                            <input className={cx('input-note')} placeholder="Ghi chú" />
                         </div>
                     </div>
-                    <div className={cx('quantity')}>
-                        <div className={cx('text-quantity')}>Số lượng</div>
-                        <button className={cx('qty-down')}>-</button>
-                        <input id={cx('qty')} value={1} />
-                        <button className={cx('qty-up')}>+</button>
-                    </div>
-                    <div className={cx('payment')}>
-                        <button className={cx('btn-buy')}>Mua ngay</button>
-                        <button className={cx('btn-addToCart')}>Thêm vào giỏ hàng</button>
-                    </div>
-                    <div className={cx('store-available')}>
-                        <FontAwesomeIcon icon={faShop} className={cx('icon-commit')} />
-                        Cửa hàng còn sản phẩm{' '}
-                    </div>
-                    <div className={cx('commit')}>
-                        <span>
-                            <FontAwesomeIcon icon={faCheck} className={cx('icon-commit')} />
-                            Hàng chính hãng 100%
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faBackspace} className={cx('icon-commit')} />
-                            Đổi ý miễn phí
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faTruck} className={cx('icon-commit')} />
-                            Miễn phí vận chuyển
-                        </span>
+                    <div className={cx('info-product')}>
+                        <div className={cx('header-product')}>
+                            <div className={cx('info-product-text')}>
+                                <div className={cx('')}>Thông tin đơn hàng</div>
+                                <button className={cx('btn-change')}>Sửa</button>
+                            </div>
+                            <div className={cx('product-name')}>
+                                NÓN BUCKET A2NBK01305 - BU <div>x1</div>
+                            </div>
+                            <div className={cx('discount')}>
+                                <FontAwesomeIcon icon={faTag} className={cx('icon-discount')}></FontAwesomeIcon>
+                                Chọn hoặc nhập mã giảm giá
+                            </div>
+                            <div className={cx('price')}>
+                                <div className={cx('price-text')}>
+                                    <p>Tổng tiền hàng</p> <span className={cx('price-product')}>150,000đ</span>
+                                </div>
+                                <div className={cx('price-text')}>
+                                    <p>Phí vận chuyển</p> <span className={cx('price-trasport')}>0đ</span>
+                                </div>
+                            </div>
+                            <div className={cx('coupon')}>
+                                <p>Giảm giá coupon</p>
+                                <span>0đ</span>
+                            </div>
+                            <div className={cx('total')}>
+                                <p>Tạm tính</p>
+                                <span className={cx('total-price')}>150,000đ</span>
+                            </div>
+                            <p>(Đã bao gồm VAT nếu có)</p>
+                        </div>
+                        <button className={cx('btn-done')}>Hoàn tất</button>
                     </div>
                 </div>
             </div>
-            <div className={cx('description')}>
-                <div className={cx('text-description')}>Mô tả sản phẩm</div>
-                <div className={cx('content-description')}>
-                    Chất liệu: Jean Kiểu dáng: Oversize Màu sắc: Xanh, Đen "Xanh & Đen", 2 gam màu đầy cá tính dành cho
-                    các FRIENDs năng động. Chiếc nón Unisex vừa cập bến nhà TOTODAY chắc chắn sẽ không khiến các FRIENDs
-                    thất vọng. Thiết kế sở hữu chất liệu Jean thoáng mát, thoải mái nhưng lại không lỗi thời, chắc chắn
-                    sẽ là một item không thể thiếu trong tủ đồ của các FRIENDs.
+            <div className={cx('payment-wrapper')}>
+                <div className={cx('payment')}>Phương thức thanh toán</div>
+                <div className={cx('cod')}>
+                    <FontAwesomeIcon icon={faCircleDot} className={cx('cod-icon')}></FontAwesomeIcon>
+                    <div className={cx('cod-logo')}>
+                        <Image
+                            className={cx('cod-image')}
+                            src="https://web.nvnstatic.net/tp/T0441/img/cod.svg?v=3"
+                            alt="COD"
+                        />
+                    </div>
+                    <div className={cx('cod-text')}>COD(Thanh toán khi nhận hàng)</div>
+                </div>
+                <div className={cx('momo')}>
+                    <FontAwesomeIcon icon={faCircleDot} className={cx('momo-icon')}></FontAwesomeIcon>
+
+                    <div className={cx('momo-logo')}>
+                        <Image
+                            className={cx('momo-image')}
+                            src="https://web.nvnstatic.net/img/logo_momo.png?v=2"
+                            alt="Momo"
+                        />
+                    </div>
+                    <div className={cx('momo-text')}>Thanh toán qua momo</div>
+                </div>
+                <div className={cx('banking')}>
+                    <FontAwesomeIcon icon={faCircleDot} className={cx('banking-icon')}></FontAwesomeIcon>
+
+                    <div className={cx('banking-logo')}>
+                        <Image
+                            className={cx('banking-image')}
+                            src="https://web.nvnstatic.net/tp/T0441/img/vnpay.svg?v=3"
+                            alt="Internet Banking"
+                        />
+                    </div>
+                    <div className={cx('banking-text')}>Thanh toán qua ngân hàng(Internet Banking)</div>
                 </div>
             </div>
         </div>
